2bdd3112f6339fb87f7b81222dbeb4527842c343
Bohdan Mitsevskyi <mitsevskyi.corporate@gmail.com>
2022-10-07 19:45:47 +0300

    Add newSell changes

--- controllers/sellController.ts
+++ controllers/sellController.ts
@@ -16,22 +16,40 @@ class createSell {
             const ItemActiveSell = await Items.findOne ({
                 order: [ [ 'createdAt', 'DESC' ]],
             });
-                
+
+            const ItemsNumber = await Items.count();
+
             if(!SellActiveShift) {
                 res.status(400).json({message: 'You need to create new Shift at first'});
             }
             else if(!ItemActiveSell){
                 res.status(400).json({message: 'You need to create new Item at first'});
             }
-            const shiftId = SellActiveShift.id;
-            const itemId = ItemActiveSell.id
-            const price = ItemActiveSell.price;
+            else if(ItemsNumber==1){
+            
+                const shiftId = SellActiveShift.id;
+                const itemId = ItemActiveSell.id
+                const price = ItemActiveSell.price;
 
-            req.body = {itemId, price};
+                req.body = {itemId, price};
                 
-            const newSell = new Sells({shiftId, itemId, price});
-            await newSell.save();
-            res.json({message:'newSell was created'});
+                const newSell = new Sells({shiftId, itemId, price});
+                await newSell.save();
+                res.json({message:'newSell was created from last ItemId automatically, because Item is ONLY one. If you want add item manually, use post method body with itemId, price'});
+            }
+
+            else if(ItemsNumber>1) {
+                const {itemId, price} = req.body;
+                if(!itemId) {
+                    res.status(400).json({message:'Please, add itemId, price IS NOT REQUIRED'});
+                }
+                else {
+                    const shiftId = SellActiveShift.id;
+                    const newSell = new Sells({shiftId, itemId, price});
+                    await newSell.save();
+                    res.json({message:'newSell was created'});
+                }
+            }
 
             }
             
