d255183cabfe4e98911dab905c23727a0ddd8c32
Bohdan Mitsevskyi <mitsevskyi.corporate@gmail.com>
2022-10-11 10:11:21 +0300

    make code more correctly

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -13,29 +13,37 @@ const generateJwt = (id: any, email: any) => {
 
 class userController {
     async registration(req: express.Request, res: express.Response) {
+    try{
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             res.status(400).json({message:'Registration Error',errors});
         }
-    else{
-    const {email, password} = req.body;
-    const isUserRegistered = await users.findOne({where:{email}});
-    if(isUserRegistered){
-        res.status(400).json({message:`User with ${email} was already registered`})
+        else {
+            const {email, password} = req.body;
+            const isUserRegistered = await users.findOne({where:{email}});
+        if(isUserRegistered) {
+            res.status(400).json({message:`User with ${email} was already registered`})
+        }
+        else {
+            console.log(isUserRegistered, email, password);
+            const hashPasword = await bcrypt.hash(password, 8);
+            const newUser = await users.create({email, password:hashPasword});
+            const token = generateJwt(newUser.id, newUser.email);
+            await newUser.save();
+            res.json({message:`New User was succesfully created with token ${token}`});
+        }
+        }
+    
     }
-    else{
-    console.log(isUserRegistered, email, password);
-    const hashPasword = await bcrypt.hash(password, 8);
-    const newUser = await users.create({email, password:hashPasword});
-    const token = generateJwt(newUser.id, newUser.email);
-    await newUser.save();
-    res.json({message:`New User was succesfully created with token ${token}`});
+    catch(e) {
+        console.log(e);
     }
-}
     }
 
     async login(req:express.Request, res: express.Response) {
-        const {email, password} = req.body;
+        
+    try {
+    const {email, password} = req.body;
         const User = await users.findOne({where:{email}});
         if(!User) {
             res.status(400).json({message:`User with this email ${email} does not exist`});
@@ -48,8 +56,12 @@ class userController {
             const token = generateJwt(User.id, User.email);
             return res.json({message:`User was succesfully logined with token ${token}`});
         }
+    }   
+    
+    catch(e) {
+    console.log(e)
     }
-
+    }
    
 }
 
